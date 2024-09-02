918bbe54d1b23726fbdaad89c62f82bd1857c044
Ariq Fikri Tamamy <ariqrick@gmail.com>
2024-09-03 06:31:47 +0700

    Pengumpulan Tugas 9

--- tugas9/lib/main.dart
+++ tugas9/lib/main.dart
@@ -1,125 +1,169 @@
+// import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:tugas9/model/news_model.dart';
+import 'package:tugas9/widget/custom_box_image.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const NewsHomePage());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
+class NewsHomePage extends StatelessWidget {
+  const NewsHomePage({super.key});
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      home: Scaffold(
+        resizeToAvoidBottomInset: false,
+        appBar: AppBar(
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              CustomBoxImage(
+                urlImage: 'assets/images/Menu Icon.png',
+                width: 40,
+                height: 40,
+              ),
+              Text(
+                'NewsApp',
+                style: GoogleFonts.poppins(
+                    fontSize: 16, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(),
+            ],
+          ),
+        ),
+        body: Column(
+          children: [
+            SizedBox(
+              height: 311,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: 3,
+                itemBuilder: (context, index) => CustomBoxImage(
+                  margin: EdgeInsets.only(top: 32, left: index == 0 ? 30 : 0),
+                  padding: const EdgeInsets.all(24),
+                  urlImage: 'assets/images/News.png',
+                  width: 311,
+                  height: 311,
+                  child: Stack(
+                    children: [
+                      Column(
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text('TECHNOLOGY',
+                                  style: GoogleFonts.poppins(
+                                      fontSize: 12,
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold)),
+                              Text('3 min ago',
+                                  style: GoogleFonts.poppins(
+                                      fontSize: 12, color: Colors.white)),
+                            ],
+                          ),
+                          const Spacer(),
+                          Text(
+                              'Microsoft launches a deepfake detector tool ahead of US election',
+                              style: GoogleFonts.poppins(
+                                  fontSize: 18,
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold)),
+                          const SizedBox(height: 24),
+                          Row(
+                            children: [
+                              CustomBoxImage(
+                                urlImage:
+                                    'assets/images/chatbubble-ellipses-outline.png',
+                                width: 20,
+                                height: 20,
+                              ),
+                              CustomBoxImage(
+                                margin: const EdgeInsets.only(left: 24),
+                                urlImage: 'assets/images/bookmark-outline.png',
+                                height: 20,
+                                width: 20,
+                              ),
+                              const Spacer(),
+                              CustomBoxImage(
+                                urlImage:
+                                    'assets/images/arrow-redo-outline.png',
+                                width: 20,
+                                height: 20,
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            Padding(
+              padding: const EdgeInsets.only(left: 30, right: 30, top: 40),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Latest News',
+                    style: GoogleFonts.poppins(
+                        fontSize: 14,
+                        color: const Color(0xff111E29).withOpacity(0.48)),
+                  ),
+                  Icon(Icons.arrow_circle_right_outlined,
+                      color: const Color(0xff111E29).withOpacity(0.48)),
+                ],
+              ),
+            ),
+            ListView.builder(
+              padding: const EdgeInsets.only(left: 30, right: 30),
+              shrinkWrap: true,
+              physics: const NeverScrollableScrollPhysics(),
+              // scrollDirection: Axis.vertical,
+              itemCount: news.length,
+              itemBuilder: (context, index) => Column(
+                children: [
+                  const SizedBox(height: 23),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      CustomBoxImage(
+                        urlImage: news[index].url,
+                        width: 100,
+                        height: 100,
+                      ),
+                      const SizedBox(width: 24),
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              news[index].title,
+                              style: GoogleFonts.poppins(
+                                  fontSize: 12,
+                                  fontWeight: FontWeight.bold,
+                                  color: const Color(0xff141E48)
+                                      .withOpacity(0.48)),
+                            ),
+                            Text(
+                              news[index].subTitle,
+                              style: GoogleFonts.poppins(
+                                  fontSize: 18, fontWeight: FontWeight.bold),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
